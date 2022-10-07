60b52585f63afea8a80ba9b91357ecaf80b889af
Jkevin <kevinjiang.366@gmail.com>
2022-10-08 02:14:19 +0800

    stream流的中间操作

---
+++ MavenRepository/MavenRepository/src/test/java/org/example/java4/Boy.java
@@ -0,0 +1,20 @@
+package org.example.java4;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ * @Description TODO
+ * @Author JKevin
+ * @Date 2022年10月06日 下午11:32
+ * @Version 1.0
+ **/
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class Boy {
+    private Girl girl;
+}
