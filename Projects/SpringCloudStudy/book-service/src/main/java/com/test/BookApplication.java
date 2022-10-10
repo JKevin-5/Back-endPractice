ce38472d2b4308c59549abe25f5d03f3f9a5fd7b
JKevin <15810880126@163.com>
2022-10-11 00:25:08 +0800

    开启springcloud的项目系统学习

---
+++ Projects/SpringCloudStudy/book-service/src/main/java/com/test/BookApplication.java
@@ -0,0 +1,15 @@
+package com.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Jkevin
+ * @date 2022年10月10日 22:56
+ */
+@SpringBootApplication
+public class BookApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookApplication.class, args);
+    }
+}
